52aa179857761a13ab47ad809397c12736e8a96b
Alvaro Rodriguez Gonzalez <alvamige@gmail.com>
2025-07-24 14:01:31 +0200

    Disable SSL for database connection

--- src/config/database.ts
+++ src/config/database.ts
@@ -12,6 +12,9 @@ const dbConfig: PoolConfig = {
   max: 20, // Maximum number of clients in the pool
   idleTimeoutMillis: 30000, // Close idle clients after 30 seconds
   connectionTimeoutMillis: 2000, // Return an error after 2 seconds if connection could not be established
+  ssl: {
+    rejectUnauthorized: false
+  }
 };
 
 export const pool = new Pool(dbConfig);
